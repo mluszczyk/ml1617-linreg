Fix in normalization.

test_regression.py
@@ -21,7 +21,7 @@ def get_y_fun(a, b, c):
 
 lin_fun = get_y_fun(a, b, c)
 
-n = 500
+n = 1000
 range_points = 1
 range_plot = 1.1
 
@@ -30,6 +30,9 @@ sigma = 0.05
 X = range_points * 2 * (np.random.rand(n, 2) - 0.5)
 
 y = [lin_rule(x, sigma * np.random.normal()) for x in X]
+from matplotlib import pyplot
+pyplot.hist(y)
+pyplot.draw()
 
 print(X[:10])
 print(y[:10])
@@ -40,9 +43,12 @@ from sklearn.metrics import accuracy_score
 clf = MyLogisticRegression()
 clf.fit(X, y)
 
-print(clf.coef_)
-print(clf.intercept_)
-
 y_pred = clf.predict(X)
 
+from matplotlib import pyplot
+pyplot.hist(clf.y_)
+pyplot.draw()
+
 print('Accuracy: {}'.format(accuracy_score(y, y_pred)))
+
+pyplot.show()