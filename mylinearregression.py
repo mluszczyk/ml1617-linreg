dcae4589cf6664c0ac974fd1026c2ef4a2fd6faa
Michał Łuszczyk <michal.luszczyk@gmail.com>
2016-11-30 18:09:28 +0100

    Base class extracted for both regressions.

--- mylinearregression.py
+++ mylinearregression.py
@@ -1,72 +1,17 @@
-from sklearn.base import BaseEstimator
-from sklearn.preprocessing import StandardScaler
-from sklearn.utils import shuffle
-from sklearn.utils.validation import check_X_y, check_array
-
-from linreg import predict, linreg, adjust, rmse_partial_derivative
+from mybaseregression import MyBaseRegression
+from linreg import rmse_partial_derivative, predict
 
 
-class MyLinearRegression(BaseEstimator):
+class MyLinearRegression(MyBaseRegression):
     """ A template estimator to be used as a reference implementation .
     Parameters
     ----------
     demo_param : str, optional
         A parameter used for demonstation of how to pass and store paramters.
     """
-    def __init__(self, batch_size=None, n_epochs=100, shuffle: bool=False,
-                 holdout_size: float=0., l2=0., learning_rate=.1, decay=1.0,
-                 standardize: bool=False):
-        self.batch_size = batch_size
-        self.n_epochs = n_epochs
-        self.shuffle = shuffle
-        self.holdout_size = holdout_size
-        self.l2 = l2
-        self.learning_rate = learning_rate
-        self.decay = decay
-        self.standardize = standardize
 
-        self.w = None
-        self.validation = None
-        self.standard_scaler = None
-
-        self.partial_derivative = rmse_partial_derivative
-        self.predict_func = predict
-
-    def holdout(self, X, y):
-        holdout_num = int(round(self.holdout_size * X.shape[0]))
-        if self.holdout_size:
-            if self.shuffle:
-                X, y = shuffle(X, y)
-            self.validation = X[:holdout_num, :], y[:holdout_num]
-            X, y = X[holdout_num:], y[holdout_num:]
-        return X, y
-
-    def fit(self, X, y):
-        """A reference implementation of a fitting function
-        Parameters
-        ----------
-        X : array-like or sparse matrix of shape = [n_samples, n_features]
-            The training input samples.
-        y : array-like, shape = [n_samples] or [n_samples, n_outputs]
-            The target values (class labels in classification, real numbers in
-            regression).
-        Returns
-        -------
-        self : object
-            Returns self.
-        """
-        X, y = check_X_y(X, y)
-        X, y = self.holdout(X, y)
-        if self.standardize:
-            self.standard_scaler = StandardScaler()
-            X = self.standard_scaler.fit_transform(X)
-        batch_size = X.shape[0] if self.batch_size is None else self.batch_size
-        self.w = linreg(
-            self.partial_derivative,
-            adjust(X), y, batch_size, self.n_epochs, self.shuffle,
-            self.l2, self.learning_rate, self.decay)
-        # Return the estimator
-        return self
+    def __init__(self, *args, **kwargs):
+        super().__init__(rmse_partial_derivative, *args, **kwargs)
 
     def predict(self, X):
         """ A reference implementation of a predicting function.
@@ -79,8 +24,4 @@ class MyLinearRegression(BaseEstimator):
         y : array of shape = [n_samples]
             Returns :math:`x^2` where :math:`x` is the first column of `X`.
         """
-        X = check_array(X)
-        if self.standard_scaler is not None:
-            X = self.standard_scaler.transform(X)
-
-        return self.predict_func(self.w, adjust(X))
+        return self.predict_wrapper(X, predict)
--- mylogisticregression.py
+++ mylogisticregression.py
@@ -1,32 +1,17 @@
-import numpy
-from sklearn.base import BaseEstimator
-from sklearn.utils import check_array
-
-from linreg import partial_derivative_logistic, predict_logistic, predict_logistic_bool, adjust
-from mylinearregression import MyLinearRegression
+from linreg import partial_derivative_logistic, predict_logistic, predict_logistic_bool
+from mybaseregression import MyBaseRegression
 
 
-class MyLogisticRegression(BaseEstimator):
+class MyLogisticRegression(MyBaseRegression):
     def __init__(self, *args, **kwargs):
-        self.linear_regression = MyLinearRegression(*args, **kwargs)
-        assert self.linear_regression.partial_derivative is not None
-        self.linear_regression.partial_derivative = partial_derivative_logistic
-        assert self.linear_regression.predict_func is not None
-        self.linear_regression.predict_func = predict_logistic_bool
+        super().__init__(partial_derivative_logistic, *args, **kwargs)
 
     def fit(self, X, y):
-        from matplotlib import pyplot
-        pyplot.hist(y)
-
-        self.linear_regression.fit(X, y)
+        super().fit(X, y)
         return self
 
     def predict(self, X):
-        return self.linear_regression.predict(X)
+        return self.predict_wrapper(X, predict_logistic_bool)
 
     def predict_log(self, X):
-        X = check_array(X)
-        if self.linear_regression.standard_scaler is not None:
-            X = self.linear_regression.standard_scaler.transform(X)
-
-        return predict_logistic(self.linear_regression.w, adjust(X))
+        return self.predict_wrapper(X, predict_logistic)
